ab24f983150e8d101986b11f084483058bd8efa9
Dharunika-07-N <dharunika0708@gmail.com>
2026-02-25 15:13:56 +0530

    chore: finalize frontend replacement and backend sqlite setup

--- frontend/src/utils/passwordStrength.js
+++
@@ -1,8 +0,0 @@
-export function passwordStrength(password){
-  const score = [/.{8,}/, /[a-z]/, /[A-Z]/, /[0-9]/, /[^A-Za-z0-9]/].reduce((s, re)=> s + (re.test(password) ? 1 : 0), 0);
-  let strength = 'Very weak';
-  if(score >= 4) strength = 'Strong';
-  else if(score === 3) strength = 'Medium';
-  else if(score === 2) strength = 'Weak';
-  return { score, strength };
-}
